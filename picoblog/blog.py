0b99f7233228dc4f30a0483b98640d854842a54d
zeencd <zeencd@594ff1ba-4ba9-08de-c13e-e67aa2b39092>
2011-07-27 12:49:50 +0000

    + markup: single mp3     + markup: mixcloud

--- picoblog/blog.py
+++ picoblog/blog.py
@@ -161,6 +161,7 @@ class AbstractPageHandler(request.BlogRequestHandler):
                     article.html = my_rst2html(article.body)
                     article.html = self.handle_custom_tag_mp3(article.html)
                     article.html = self.handle_custom_tag_youtube(article.html)
+                    article.html = self.handle_custom_tag_mixcloud(article.html)
                 except AttributeError, e:
                     #article.html = 'an error occured performing rst2html()'
                     raise e
@@ -180,16 +181,28 @@ class AbstractPageHandler(request.BlogRequestHandler):
     def handle_custom_tag_mp3(self, input):
         dropbox_user = "1883230"
 
-        regex = "\[mp3 +([^\]]+)]"
+        #regex = "\[mp3 +([^\]]+)]"
+        regex = "\[([^\]]+mp3)\]"
 
         repl_link  = "http://dl.dropbox.com/u/%s/sg/\\1" % (dropbox_user)
         repl_swf   = "http://dl.dropbox.com/u/%s/dewplayer-mini.swf" % (dropbox_user)
-        repl_flash = "<object width='160' height='20'><embed src='" + repl_swf + "' width='160' height='20' type='application/x-shockwave-flash' flashvars='&mp3=" + repl_link + "' quality='high'></embed></object>"
+        repl_flash = "<object width='160' height='18'><embed src='" + repl_swf + "' width='160' height='18' type='application/x-shockwave-flash' flashvars='&mp3=" + repl_link + "' quality='high'></embed></object>"
         repl_dload = "<a href=\"%s\">\\1</a>" % (repl_link)
         repl = "%s %s" % (repl_flash, repl_dload)
 
         import re
         return re.sub(regex, repl, input)
+
+    def handle_custom_tag_mixcloud(self, input):
+        dropbox_user = "1883230"
+
+        #regex = "\[mp3 +([^\]]+)]"
+        regex = "\[\s*(http://)?(www.)?mixcloud.com/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)/?\s*\]"
+
+        replace  = '<object height="400" width="400"><param name="movie" value="http://www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2F\\3%2F\\4%2F&amp;embed_uuid=3b4627b1-74e1-43ef-bc52-717acca644d4&amp;embed_type=widget_standard"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"><embed src="http://www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2F\\3%2F\\4%2F&amp;embed_uuid=3b4627b1-74e1-43ef-bc52-717acca644d4&amp;embed_type=widget_standard" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" height="400" width="400"></object>'
+
+        import re
+        return re.sub(regex, replace, input)
 
     def render_articles(self,
                         articles,
