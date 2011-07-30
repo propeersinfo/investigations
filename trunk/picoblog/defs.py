ac8809e8481ff7035d07d1d7f41aabf3cf8d5e9a
zeencd <zeencd@594ff1ba-4ba9-08de-c13e-e67aa2b39092>
2011-07-30 11:08:09 +0000

    templates/static moved to a theme

--- trunk/picoblog/defs.py
+++ trunk/picoblog/defs.py
@@ -1,39 +1,27 @@
-# $Id: 51015724daff7e7df2558e0f6ff8222dbaa30749 $
-#
-# Constants used by this application
-
-# -----------------------------------------------------------------------------
-# Imports
-# -----------------------------------------------------------------------------
-
 import os
 
-# -----------------------------------------------------------------------------
-# Constants
-# -----------------------------------------------------------------------------
-
-CANONICAL_BLOG_URL = 'http://localhost:8080/'
 BLOG_NAME = 'Soviet Rare Groove'
 BLOG_OWNER = 'Soviet Groove'
 
-DROPBOX_USER = "1883230"
+DROPBOX_USER = '1883230'
 
-TEMPLATE_SUBDIR = 'templates'
+THEME_DIR = 'themes/birman'
+TEMPLATE_DIR = 'templates'
 
 TAG_URL_PATH = 'tag'
 DATE_URL_PATH = 'date'
 #ARTICLE_URL_PATH = 'id'
 MEDIA_URL_PATH = 'static'
 ATOM_URL_PATH = 'atom'
-RSS2_URL_PATH = 'rss2'
+RSS2_URL_PATH = 'rss'
 ARCHIVE_URL_PATH = 'archive'
 
 MAX_ARTICLES_PER_PAGE = 10
 TOTAL_RECENT = 10
 
-_server_software = os.environ.get('SERVER_SOFTWARE','').lower()
-if _server_software.startswith('goog'):
-    ON_GAE = True
+if os.environ.get('SERVER_SOFTWARE','').lower().find('development') >= 0:
+  PRODUCTION = False
+  CANONICAL_BLOG_URL = 'http://localhost:8080/'
 else:
-    ON_GAE = False
-del _server_software
+  PRODUCTION = True
+  CANONICAL_BLOG_URL = 'http://sovietraregroove.appspot.com/'
