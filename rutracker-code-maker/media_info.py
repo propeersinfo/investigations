aee1d5705ccc85b2ab108215bcdbd8788e3f367c
zeencd <zeencd@594ff1ba-4ba9-08de-c13e-e67aa2b39092>
2011-09-18 20:45:17 +0000

    media info is collected correctly

--- rutracker-code-maker/media_info.py
+++ rutracker-code-maker/media_info.py
@@ -1,6 +1,16 @@
 import os
+import mutagen
 from mutagen.mp3 import MP3
 
+def get_single_audio_info(file):
+    try:
+        audio = MP3(file.path)
+        return audio.info.length, audio.info.bitrate
+    #except mutagen.mp3.HeaderNotFoundError:
+    except RuntimeError:
+        return (0, 0)
+
+"""
 def get_audio_info(files):
 	total_length = 0
 	total_bitrate = 0
@@ -15,3 +25,4 @@ def get_audio_info(files):
 		return (total_length, total_bitrate / 1000 / total_bitrate_count)
 	else:
 		return (0, 0)
+"""
