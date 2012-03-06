94b6d7a148250a77455e77eae87d298c2f602f08
zeencd <zeencd@594ff1ba-4ba9-08de-c13e-e67aa2b39092>
2012-03-06 02:38:24 +0000

    +tag cloud enhanced +full tag listing page

--- picoblog/my_tags.py
+++ picoblog/my_tags.py
@@ -10,21 +10,24 @@ register = webapp.template.create_template_register()
 
 ###################
 
+# print a tag
 def blog_tag(blog_tag_name, blog_tag_title = None):
   if not blog_tag_title:
     blog_tag_title = blog_tag_name
   return '<a href="/tag/%s">%s</a>' % (blog_tag_name, blog_tag_title)
 register.simple_tag(blog_tag)
 
-def blog_tag_2(tag_cloud, blog_tag_name, blog_tag_title = None):
+# print a tag with its usage count
+def blog_tag_cnt(tag_cloud, blog_tag_name, blog_tag_title = None):
+  #raise Exception('%s' % type(tag_cloud))
   if not blog_tag_title:
     blog_tag_title = blog_tag_name
-  count = tag_cloud.get(blog_tag_name, 0)
+  count = tag_cloud.get_tag_usage_count(blog_tag_name)
   if count > 0:
     return '<a href="/tag/%s">%s</a> <span>%s</span>' % (blog_tag_name, blog_tag_title, count)
   else:
-    return '%s' % (blog_tag_title)
-register.simple_tag(blog_tag_2)
+    return '%s' % blog_tag_title
+register.simple_tag(blog_tag_cnt)
 
 ###################
 
@@ -34,23 +37,23 @@ register.simple_tag(h2_render)
 
 ###################
 
-def mytag(parser, token):
-    bits = list(token.split_contents())
-    if len(bits) != 2:
-        raise django_template.TemplateSyntaxError, "Error: len(bits) = %s" % len(bits)
-    return MyNode(bits[1])
-
-class MyNode(django_template.Node):
-    def __init__(self, my_var):
-        self.my_var = my_var
-    def render(self, context):
-        try:
-            my_var = django_template.resolve_variable(self.my_var, context)
-        except django_template.VariableDoesNotExist:
-            my_var = None
-        return "my var is: %s" % my_var
-
-register.tag(mytag)
+#def mytag(parser, token):
+#    bits = list(token.split_contents())
+#    if len(bits) != 2:
+#        raise django_template.TemplateSyntaxError, "Error: len(bits) = %s" % len(bits)
+#    return MyNode(bits[1])
+#
+#class MyNode(django_template.Node):
+#    def __init__(self, my_var):
+#        self.my_var = my_var
+#    def render(self, context):
+#        try:
+#            my_var = django_template.resolve_variable(self.my_var, context)
+#        except django_template.VariableDoesNotExist:
+#            my_var = None
+#        return "my var is: %s" % my_var
+#
+#register.tag(mytag)
 
 ###################
 
