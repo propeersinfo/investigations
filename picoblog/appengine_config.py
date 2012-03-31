e5bc544f1a211aede51b0dbda54df2cc5d8200b0
zeencd <zeencd@594ff1ba-4ba9-08de-c13e-e67aa2b39092>
2012-03-31 18:18:42 +0000

    different changes

--- picoblog/appengine_config.py
+++ picoblog/appengine_config.py
@@ -1,10 +1,27 @@
-using_profiler = True
+#using_profiler = True
+#
+#from gae_mini_profiler import profiler
+#gae_mini_profiler_ENABLED_PROFILER_EMAILS = ['zeencd@gmail.com', 'test@example.com']
+#
+#def webapp_add_wsgi_middleware(app):
+#    """Called with each WSGI handler initialisation"""
+#    #app = gae_mini_profiler.profiler.ProfilerWSGIMiddleware(app)
+#    app = profiler.ProfilerWSGIMiddleware(app)
+#    return app
 
-from gae_mini_profiler import profiler
-gae_mini_profiler_ENABLED_PROFILER_EMAILS = ['zeencd@gmail.com', 'test@example.com']
+appstats_MAX_STACK = 20
 
+#def appstats_normalize_path(path):
+#    return path
+#
+#def appstats_extract_key(request):
+#    key = appstats_normalize_path(request.http_path())
+#    #if request.http_method() != 'GET':
+#    key = '!!!!!!!!!!!!!!! %s %s' % (request.http_method(), key)
+#    return key
+
+# appstats
 def webapp_add_wsgi_middleware(app):
-    """Called with each WSGI handler initialisation"""
-    #app = gae_mini_profiler.profiler.ProfilerWSGIMiddleware(app)
-    app = profiler.ProfilerWSGIMiddleware(app)
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
     return app
